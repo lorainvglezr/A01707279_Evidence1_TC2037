cacad3e3023545d78925d0dd2628fb21e8382b12
lorainvglezr <A01707279@tec.mx>
2025-03-24 12:14:11 -0600

    [UPDATE] comments in 'evidence1.pl' file

--- evidence1.pl
+++ evidence1.pl
@@ -1,4 +1,4 @@
-%Definition of data base
+% Definition of knowledge base
 displace(q0, q1, k).
 displace(q1, q2, a).
 displace(q2, q4, l).
@@ -21,24 +21,24 @@ displace(q14, q15, a).
 displace(q15, qf, n).
 
 
-%Specify the automaton's final state (accepting state)
+% Specify the automaton's final state (accepting state)
 final_state(qf).
 
-%Set initial state and pass it to the next function
+% Set initial state and pass it to the next function
 journey_automaton(Term):-
     correction_agent(Term, q0).
 
-%Base case, the entire word is checked and verifies if it ends in an accepted state
+% Base case, the entire word is checked and verifies if it ends in an accepted state
 correction_agent([], Initial_state):-
     final_state(Initial_state).
 
-%Function that continues while the list contains elements
+% Function that continues while the list contains elements
 correction_agent([Word_letter | Word_tail], Initial_state):-
     displace(Initial_state, Next_step, Word_letter),
     correction_agent(Word_tail, Next_step).
 
 
-%Several test cases
+% Several test cases
 kala:-
     write('kala'), nl,
     write('Automaton travel: TRUE'), nl,
